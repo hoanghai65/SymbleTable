49c3600d290b2c555ac976da18e43969916a9db0
Hoàng Đức Hải <84216012+hoanghai65@users.noreply.github.com>
2021-10-12 10:54:47 +0700

    Update AbstractSymbleTable.java

--- AbstractSymbleTable.java
+++ AbstractSymbleTable.java
@@ -1,6 +1,6 @@
 
 
-public abstract class AbstractSymbleTable<Key extends Comparable<Key>, Value> implements SymbleTable<Key,Value> {
+public abstract class AbstractSymbaleTable<Key extends Comparable<Key>, Value> implements SymbleTable<Key,Value> {
         @Override
         public boolean isEmpty() {
             return size() == 0;
